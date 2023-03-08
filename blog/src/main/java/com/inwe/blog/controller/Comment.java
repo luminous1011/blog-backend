d13d8923d998f2196b7a0389bc248bc271f2e609
panjinshan <pjspan@foxmail.com>
2023-03-08 11:00:02 +0800

    @author luminous 评论接口添加分页

--- blog/src/main/java/com/inwe/blog/controller/Comment.java
+++ blog/src/main/java/com/inwe/blog/controller/Comment.java
@@ -1,7 +1,10 @@
 package com.inwe.blog.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.inwe.blog.dao.CommentMapper;
 import com.inwe.blog.dao.UserMapper;
+import com.inwe.blog.model.Essay;
 import com.inwe.blog.model.User;
 import com.inwe.blog.utlis.R;
 import com.inwe.blog.utlis.Util;
@@ -21,21 +24,30 @@ public class Comment {
     @Autowired
     UserMapper userMapper;
 
+    /*
+     * 新增评论
+     * */
     @PostMapping ("addComment")
     public R addComment(@RequestBody Map<String,String> map){
         String uid = insertUser(map);
         insertComment(uid,map);
         return  R.ok();
     }
+    /*
+     * 回复评论
+     * */
     @PostMapping("replyComment")
     public R replyComment(@RequestBody Map<String,String> map){
         String uid = insertUser(map);
         insertComment(uid,map);
         return R.ok();
     }
+    /*
+     * 获取comment列表
+     * */
     @GetMapping ("getCommentsList")
-    public R getCommentsList(String path){
-        List<com.inwe.blog.model.Comment> data = getCommentList("comment",path);
+    public R getCommentsList(String path,int page,int pageSize){
+        PageInfo<com.inwe.blog.model.Comment> data = getCommentList("comment",path, page, pageSize);
         return R.ok().put("data",data);
     }
     public List<com.inwe.blog.model.Comment> getCommentReplyList(com.inwe.blog.model.Comment commentVar){
@@ -50,13 +62,14 @@ public class Comment {
         return comments;
     }
 
-    public List<com.inwe.blog.model.Comment> getCommentList(String replyType,String path){
+    public PageInfo getCommentList(String replyType,String path,int page,int pageSize){
+        PageHelper.startPage(page, pageSize);
         List<com.inwe.blog.model.Comment> data = commentMapper.getCommentList(replyType,path);
         data.forEach((com.inwe.blog.model.Comment comment)->{
             comment.setFromUser(userMapper.selectById(comment.getFromUid()));
             comment.setChildren(getCommentReplyList(comment));
         });
-        return  data;
+        return  new PageInfo<>(data);
     }
 
     /*
